6e97c500cdc897bb783c53f661d002af5c3d89b7
Zachary Pratt <zpratt65@gmail.com>
2026-02-24 15:29:17 -0500

    Implement CalculateJoist procedure and update API endpoint to /calculate-joist

--- JoistAPI/JoistAPI.dpr
+++ JoistAPI/JoistAPI.dpr
@@ -1,22 +1,48 @@
+program JoistAPI;
+
 {$MODE DELPHI}{$H+}
 
 uses
   {$IFDEF UNIX}
   cthreads,
   {$ENDIF}
+  sysutils,
+  fpjson,
+  jsonparser,
   Horse,
-  Horse.CORS,
-  SysUtils;
+  Horse.CORS;
 
-procedure GetPing(Req: THorseRequest; Res: THorseResponse; Next: TNextProc);
+procedure CalculateJoist(Req: THorseRequest; Res: THorseResponse; Next: TNextProc);
+var
+  ReqBody: TJSONObject;
+  ResBody: TJSONObject;
+  Span: Double;
+  Designation: string;
 begin
-  Res.Send('Pong');
+  ReqBody := TJSONObject(GetJSON(Req.Body));
+  try
+    Span := ReqBody.Get('span', 0.0);
+
+    if Span <= 20 then
+      Designation := '10K1'
+    else
+      Designation := '12K3';
+
+    ResBody := TJSONObject.Create;
+    ResBody.Add('recommendedJoist', Designation);
+
+    Res.Send(ResBody.AsJSON);
+  finally
+    ReqBody.Free;
+    ResBody.Free;
+  end;
 end;
 
 begin
   THorse.Use(CORS);
 
-  THorse.Get('/ping', GetPing);
+  THorse.Post('/calculate-joist', CalculateJoist);
 
+  WriteLn('Joist API listening on port 9000');
   THorse.Listen(9000);
 end.
